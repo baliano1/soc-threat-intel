dec44366b1a66186678a3aef51331643df69a735
Simone Azzola <simone.azzola1@gmail.com>
2026-06-30 22:45:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -284,13 +284,13 @@ else:
                     st.write("Nessun IoC rilevato nel testo.")
                 
                 st.divider()
-                st.markdown("**Vettore Iniziale:**")
+                st.markdown("#### 1️⃣ Vettore Iniziale")
                 st.write(a.get('vettore_attacco'))
                 
-                st.markdown("**Tecnica Exploit:**")
+                st.markdown("#### 💣Tecnica Exploit")
                 st.write(a.get('tecnica_exploit'))
                 
-                st.markdown("**Impatto sui Sistemi:**")
+                st.markdown("#### 💥Impatto sui Sistemi")
                 st.write(a.get('impatto_tecnico'))
 
             st.markdown("#### 🛡️ Raccomandazioni")
